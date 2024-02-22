46f148e7f9ebf8e2504bcae773230abbe70f74fb
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2024-02-22 22:09:33 +0000

    Test Drive Fruit Repository

---
+++ lib/fruit_repository.py
@@ -0,0 +1,29 @@
+from lib.fruit import Fruit
+
+class FruitRepository():
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM fruits')
+        fruits = []
+
+        for row in rows:
+            fruit = Fruit(row['id'], row['name'], row['calory'])
+            fruits.append(fruit)
+        return fruits
+    
+    def find(self, fruit_id):
+        rows = self._connection.execute('SELECT * FROM fruits WHERE id = %s', [fruit_id])
+
+        row = rows[0]
+        fruit = Fruit(row['id'], row['name'], row['calory'])
+        return fruit
+    
+    def create(self, fruit):
+        self._connection.execute('INSERT INTO fruits(name, calory) VALUES (%s, %s)', [fruit.name, fruit.calory])
+        return None
+    
+    def delete(self, fruit_id):
+        self._connection.execute('DELETE FROM fruits WHERE id = %s', [fruit_id])
+        return None
---
+++ tests/test_fruit_repository.py
@@ -0,0 +1,51 @@
+from lib.fruit_repository import FruitRepository
+from lib.fruit import Fruit
+
+def test_get_all_records(db_connection): # See conftest.py to learn what `db_connection` is.
+    db_connection.seed("seeds/fruit_store.sql") # Seed our database with some test data
+    repository = FruitRepository(db_connection) # Create a new BookRepository
+
+    fruits = repository.all()
+
+    # Assert on the results
+    assert fruits == [
+        Fruit(1, "Apple", 15),
+        Fruit(2, "Pear", 1),
+        Fruit(3, "Orange", 3),
+        Fruit(4, "Mango", 8),
+        Fruit(5, "Kiwi", 25)
+    ]
+
+def test_get_one_fruit_by_id(db_connection): # See conftest.py to learn what `db_connection` is.
+    db_connection.seed("seeds/fruit_store.sql") # Seed our database with some test data
+    repository = FruitRepository(db_connection) # Create a new BookRepository
+
+    fruit = repository.find(3)
+
+    # Assert on the results
+    assert fruit == Fruit(3, "Orange", 3)
+
+def test_create(db_connection): # See conftest.py to learn what `db_connection` is.
+    db_connection.seed("seeds/fruit_store.sql") # Seed our database with some test data
+    repository = FruitRepository(db_connection) # Create a new BookRepository
+    
+    fruit = Fruit(None, 'Strawberry', 23)
+    repository.create(fruit)
+    fruit.id = 6
+    fruit_created = repository.find(6)
+    assert fruit_created == fruit
+
+def test_delete(db_connection):
+    db_connection.seed("seeds/fruit_store.sql") # Seed our database with some test data
+    repository = FruitRepository(db_connection)
+
+    repository.delete(6)
+    fruits = repository.all()
+
+    assert fruits == [
+        Fruit(1, "Apple", 15),
+        Fruit(2, "Pear", 1),
+        Fruit(3, "Orange", 3),
+        Fruit(4, "Mango", 8),
+        Fruit(5, "Kiwi", 25)
+    ]
